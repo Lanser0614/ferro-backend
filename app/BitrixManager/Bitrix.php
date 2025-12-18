462c135c68eb53ae0f43efd9a64ac5ae8cbd6ca9
Anvarov <doniyor.anvarov@bellissimo.uz>
2025-12-18 20:26:55 +0500

    fix class

--- app/BitrixManager/Bitrix.php
+++ app/BitrixManager/Bitrix.php
@@ -2,26 +2,18 @@
 
 namespace App\BitrixManager;
 
+use Illuminate\Support\Facades\Http;
+
 class Bitrix
 {
     public function sendDataToBitrix($method, $data)
     {
-        $queryUrl = config('bitrix24.base_uri') . $method;
-        $queryData = http_build_query($data);
+       return Http::withoutVerifying()
+            ->baseUrl(config('bitrix24.base_uri'))
+            ->post($method, $data)
+            ->json();
 
-        $curl = curl_init();
-        curl_setopt_array($curl, [
-            CURLOPT_SSL_VERIFYPEER => 0,
-            CURLOPT_POST => 1,
-            CURLOPT_HEADER => 0,
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => $queryUrl,
-            CURLOPT_POSTFIELDS => $queryData,
-        ]);
 
-        $result = curl_exec($curl);
-        curl_close($curl);
 
-        return json_decode($result);
     }
 }
