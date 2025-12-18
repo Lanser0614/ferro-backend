dd9319e145e384418a869b84a91f904c25101142
Anvarov <doniyor.anvarov@bellissimo.uz>
2025-12-18 13:38:16 +0500

    remove basic auth from api

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,14 @@ Route::get('/', function (\Doniyor\Bitrix24\Bitrix24Manager $manager) {
     dd($manager->crm()->deals()->get(30));
 });
 
-Route::middleware('docs.basic')->group(function () {
-    Route::view('/docs/api', 'docs.redoc')->name('docs.redoc');
+Route::view('/docs/api', 'docs.redoc')->name('docs.redoc');
 
-    Route::get('/docs/openapi.yaml', function () {
-        $specPath = base_path('docs/openapi.yaml');
+Route::get('/docs/openapi.yaml', function () {
+    $specPath = base_path('docs/openapi.yaml');
 
-        abort_unless(file_exists($specPath), 404);
+    abort_unless(file_exists($specPath), 404);
 
-        return response()->file($specPath, [
-            'Content-Type' => 'application/yaml',
-        ]);
-    })->name('docs.openapi');
-});
+    return response()->file($specPath, [
+        'Content-Type' => 'application/yaml',
+    ]);
+})->name('docs.openapi');
